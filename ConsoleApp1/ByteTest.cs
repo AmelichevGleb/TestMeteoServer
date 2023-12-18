4be61dd4d8437a4a848356e4348157f055e7564d
ASUP\Амеличев_ГЭ <amelichev.g2020@mail.ru>
2023-12-18 15:08:27 +0300

    добавлен модуль для тестирования байт поправлена основная программа

---
+++ ConsoleApp1/ByteTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class ByteTest
+    {
+        byte[] Message = new byte[] { 0x01, 0x03, 0x00, 0x00, 0x00, 0x5A, 0xC5, 0xF1 };  // правильный массив
+        byte[] Message2 = new byte[8]; //Тестовый массив для команды запроса 
+
+        /*
+            1 <---> 1
+            3 <---> 3
+            0 <---> 0
+            0 <---> 0
+            0 <---> 0
+            90 <---> 90
+            197 <---> 197
+            241 <---> 241
+        */
+
+        public bool TestСomparisonByte(byte[] _message)
+        {
+            if (Message.Length == _message.Length)
+            {
+                var temp = 0;
+                for (int i = 0; i < Message.Length; i++)
+                {
+                    Console.WriteLine(Message[i] + " <---> " + _message[i]);
+                    if (Message[i] == _message[i])
+                    {
+                        temp++;
+                    }
+                }
+                if (temp == _message.Length) { return true; }
+                else { return false; }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool TestCompletion()
+        {
+            for (int i = 0;i< Message2.Length;i++)
+            {
+                Message2[i]= Convert.ToByte(Console.ReadLine());
+              //  Console.WriteLine(Message[i] + " <---> " + Message2[i]);
+
+            }
+            Console.WriteLine("ТЕСТ BT 2 Array Output, Size: {0} Data: " + BitConverter.ToString(Message2), Message2.Length);
+            return TestСomparisonByte(Message2);
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,70 +1,116 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Reflection.Emit;
 using System.Text;
 
 namespace ConsoleApp1
 {
     internal class Program
     {
+        private static PortScanner portScanner = new PortScanner();
+        private static ByteTest _bT = new ByteTest();
+
+
+        private static bool exit = true;
+
         static void Main(string[] args)
         {
-            // Устанавливаем для сокета локальную конечную точку
-            IPHostEntry ipHost = Dns.GetHostEntry("localhost");
-            IPAddress ipAddr = ipHost.AddressList[0];
-            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 11000);
-            byte[] Message = new byte[] { 0x01,0x03 , 0xB4, 0x82, 0x80, 0x00, 0x00, 0x00, 0x00, 0x09, 0xA1, 0x27,0x1D, 0x00, 0x19, 0x00, 0x00, 0x01, 0x01, 0x00, 0x1D, 0x00, 0x00, 0x00, 0x92 };
-            // Создаем сокет Tcp/Ip
-            Socket sListener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
-            // Назначаем сокет локальной конечной точке и слушаем входящие сокеты
-            try
+            while (exit)
             {
-                sListener.Bind(ipEndPoint);
-                sListener.Listen(10);
+                Console.WriteLine("1 - тесты байтов");
+                Console.WriteLine("2 - тесты портов");
+                Console.WriteLine("3 - тестовая метеостанция");
+                Console.WriteLine("4 - выход");
+                Console.Write("ввод символа: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
 
-                // Начинаем слушать соединения
-                while (true)
+                switch (choice)
                 {
-                    Console.WriteLine("Ожидаем соединение через порт {0}", ipEndPoint);
-
-                    // Программа приостанавливается, ожидая входящее соединение
-                    Socket handler = sListener.Accept();
-                    string data = null;
-
-                    // Мы дождались клиента, пытающегося с нами соединиться
-
-                    byte[] bytes = new byte[1024];
-                    int bytesRec = handler.Receive(bytes);
-
-                    data += Encoding.UTF8.GetString(bytes, 0, bytesRec);
-
-                    // Показываем данные на консоли
-                    Console.WriteLine("РЕЗУЛЬТАТ !!!!   Array Output, Size: {0} Data: " + BitConverter.ToString(bytes), bytes.Length);
-
-                    // Отправляем ответ клиенту\
-                    string reply = "Спасибо за запрос в " + data.Length.ToString()
-                            + " символов";
-                    byte[] msg = Encoding.UTF8.GetBytes(reply);
-                    handler.Send(Message);
-
-                    if (data.IndexOf("<TheEnd>") > -1)
-                    {
-                        Console.WriteLine("Сервер завершил соединение с клиентом.");
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("тест байт");
+                        byte[] _Message = new byte[] { 0x01, 0x03, 0xB4, 0x82, 0x80 };
+                        Console.WriteLine(_bT.TestCompletion());
+                        //Console.WriteLine(_bT.testByte(_Message));
+                        Console.WriteLine("Нажмите любую кнопку...");
+                        Console.ReadLine();
+                        Console.Clear();
                         break;
-                    }
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("тест портов");
+                        Console.WriteLine("Введите IP устройства для проверки");
+                        string IP = Console.ReadLine();
+                        portScanner.Scanner(IP);
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Console.WriteLine("Тестовый сервер: ");
+                        // Устанавливаем для сокета локальную конечную точку
+                        IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+                        IPAddress ipAddr = ipHost.AddressList[0];
+                        IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 11000);
+                        byte[] Message = new byte[] { 0x01, 0x03, 0xB4, 0x82, 0x80, 0x00, 0x00, 0x00, 0x00, 0x09, 0xA1, 0x27, 0x1D, 0x00, 0x19, 0x00, 0x00, 0x01, 0x01, 0x00, 0x1D, 0x00, 0x00, 0x00, 0x92 };
+                        // Создаем сокет Tcp/Ip
+                        Socket sListener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
-                    handler.Shutdown(SocketShutdown.Both);
-                    handler.Close();
+                        // Назначаем сокет локальной конечной точке и слушаем входящие сокеты
+                        try
+                        {
+                            sListener.Bind(ipEndPoint);
+                            sListener.Listen(10);
+
+                            // Начинаем слушать соединения
+                            while (true)
+                            {
+                                Console.WriteLine("Ожидаем соединение через порт {0}", ipEndPoint);
+
+                                // Программа приостанавливается, ожидая входящее соединение
+                                Socket handler = sListener.Accept();
+                                string data = null;
+
+                                // Мы дождались клиента, пытающегося с нами соединиться
+
+                                byte[] bytes = new byte[1024];
+                                int bytesRec = handler.Receive(bytes);
+
+                                data += Encoding.UTF8.GetString(bytes, 0, bytesRec);
+
+                                // Показываем данные на консоли
+                                Console.WriteLine("РЕЗУЛЬТАТ !!!!   Array Output, Size: {0} Data: " + BitConverter.ToString(bytes), bytes.Length);
+
+                                // Отправляем ответ клиенту\
+                                string reply = "Спасибо за запрос в " + data.Length.ToString()
+                                        + " символов";
+                                byte[] msg = Encoding.UTF8.GetBytes(reply);
+                                handler.Send(Message);
+
+                                if (data.IndexOf("<TheEnd>") > -1)
+                                {
+                                    Console.WriteLine("Сервер завершил соединение с клиентом.");
+                                    break;
+                                }
+
+                                handler.Shutdown(SocketShutdown.Both);
+                                handler.Close();
+                            }
+                        }
+
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.ToString());
+                        }
+                        finally
+                        {
+                            Console.ReadLine();
+                        }
+                        break;
+                    case 4:
+                        exit = false;
+                        break;
                 }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
-            finally
-            {
-                Console.ReadLine();
             }
         }
     }
