dbdc7d5827330cf3b6815e4529057a29ac47d96c
ASUP\Амеличев_ГЭ <amelichev.g2020@mail.ru>
2023-12-18 11:49:17 +0300

    Добавлен сканер портов

---
+++ ConsoleApp1/PortScanner.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class PortScanner
+    {
+        private static Files file = new Files();
+        private static string nameFile = "port.txt"; 
+
+        public void Scanner(string IP)
+        {
+            file.CreateFile(nameFile);
+            Console.Write("начальное значение: ");
+            int startPort = Convert.ToInt32(Console.ReadLine());
+            Console.Write("конечное значение: ");
+            int endPort = Convert.ToInt32(Console.ReadLine());
+            for (int i = startPort; i <= endPort; i++)
+            {
+                using (TcpClient Scan = new TcpClient())
+                {
+                    try
+                    {
+                        Scan.Connect(IP, i);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"[{i}] | OPEN");
+                        string temp = IP +"  "+ i.ToString() +   "| OPEN "+ DateTime.Now;
+                        file.ReadFile(nameFile, temp);
+                        Console.ResetColor();
+                    }
+                    catch
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"[{i}] | CLOSED");
+                        string temp = IP + "  " + i.ToString() + "| CLOSED " + DateTime.Now;
+                        file.ReadFile(nameFile, temp);
+                        Console.ResetColor();
+                    }
+                }
+            }
+            Console.WriteLine("Нажмите любую кнопку...");
+            Console.ReadLine();
+        }
+    }
+}
